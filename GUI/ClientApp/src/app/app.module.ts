fda8e29406f2ec46906e0662f45161f0e5329884
Martinez Muñoz Jhonatan Stevens <jstevensmartinez@unicesar.edu.co>
2020-04-03 15:59:34 -0500

    Proyecto en blanco

---
+++ GUI/ClientApp/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { APP_ROUTING } from './app.routes';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    APP_ROUTING
+  ],
+  providers: [
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
