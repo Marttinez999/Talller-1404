469e6b8536c0cd7ef6f43acb3195b18eab6a6626
Martinez Muñoz Jhonatan Stevens <jstevensmartinez@unicesar.edu.co>
2020-04-03 21:50:10 -0500

    0.2.1 funcionalidad

---
+++ GUI/Controllers/PersonaController.cs
@@ -0,0 +1,12 @@
+namespace GUI.Controllers
+{
+    public class PersonaController
+    {
+        [ApiController]
+        [Route{"[Controller]"}]
+        
+        public PersonaController(){}
+
+        
+    }
+}
